30882fe429bd0e8515288dde1e416ef43b9bdef7
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-08-27 19:14:34 +0530

    refactored the themes to have button colors as inner object

--- code/src/components/styled/styled-button.js
+++ code/src/components/styled/styled-button.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: ${({ theme }) => theme.colors.toggleBackgrnd};
+  background-color: ${({ theme }) => theme.colors.button.background};
   border: 1px solid transparent;
-  color: ${({ theme }) => theme.colors.toggleText};
+  color: ${({ theme }) => theme.colors.button.text};
   border-radius: .25rem;
   cursor: pointer;
   font-size:0.8rem;
--- code/src/theme/theme.js
+++ code/src/theme/theme.js
@@ -3,8 +3,10 @@ export const darkTheme = {
     colors: {
       body: "#000000",
       text: "#FFFFFF",
-      toggleText: "#000000",
-      toggleBackgrnd: "#FFFFFF"
+      button: {
+        text: "#000000",
+        background: "#FFFFFF"
+      }
     }
 };
 
@@ -12,8 +14,10 @@ export const lightTheme = {
   colors: {
     body: "#FFFFFF",
     text: "#000000",
-    toggleText: "#FFFFFF",
-    toggleBackgrnd: "#000000"
+    button: {
+      text: "#FFFFFF",
+      background: "#000000"
+    }
   }
 };
 
